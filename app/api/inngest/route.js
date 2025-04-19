21c90a023ccbd632ea98be5e4c4892716dc3ef6c
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-04-19 12:36:15 +0600

    up

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,6 +5,7 @@ import { inngest, syncUserCreation, syncUserDelitaion, syncUserUpdation } from "
     client: inngest,
     functions: [
         syncUserCreation,
+        
         syncUserDelitaion,
         syncUserUpdation,
      ],
